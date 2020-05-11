f8874f1313534c05fcd8e4c0f12d972c287b1c10
David Lee <david.lee.sl2634@yale.edu>
2020-05-11 23:39:41 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,20 @@
 # Write your methods here
+
+def loop_message_five_times(message)
+  counter = 0 
+  message = "Hello World."
+  while counter < 5 do 
+    puts message 
+    counter += 1 
+  end 
+end
+
+def loop_message_n_times(message, integer)
+  counter = 0 
+  message = "Hello Moon."
+  while counter < integer do 
+    puts message
+    counter += 1 
+  end 
+end 
+  
